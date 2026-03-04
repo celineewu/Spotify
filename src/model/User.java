3fc547b00dd6fcb7494fce5123eb7be63c4b9e97
mtothejensen <moje1000@stud.ek.dk>
2026-03-04 11:11:15 +0100

    Superclass v1

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,38 @@
-package model;
+//package placeholder;
 
-public class User {
+/*
+ Abstract superclass.
+ Encapsulates shared state and enforces polymorphic behavior.
+*/
+
+public abstract class User {
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+
+    }
+
+    // Getters and setters (encapsulation principle)
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    /*
+     Polymorphic method.
+     Each subclass decides formatting behavior.
+    */
+    public abstract String Ads();
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
