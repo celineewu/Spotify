f473fdfcb17e2cade08d9717c236e44320900543
AmandaHult98 <amhu1000@stud.ek.dk>
2026-03-04 11:41:00 +0100

    Song

--- src/model/Song.java
+++ src/model/Song.java
@@ -2,14 +2,41 @@ package model;
 
 public class Song implements Comparable<Song>{
     private String title;
+    private Genre genre;
+    private double time;
 
-    public Song(String title) {
+    public Song(String title, Genre genre, double time) {
         this.title = title;
+        this.genre = genre;
+        this.time = time;
     }
 
+    //Getters
     public String getTitle() {
         return title;
     }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    //Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
 
     @Override
     public int compareTo(Song other) {
@@ -18,6 +45,6 @@ public class Song implements Comparable<Song>{
 
     @Override
     public String toString() {
-        return title;
+        return "Title: " + title + "\nGenre: " + genre + "\nTime: " + time;
     }
 }
