030908b5adc3f93bcc369e9cad6898da33d551cd
AmandaHult98 <amhu1000@stud.ek.dk>
2026-03-04 10:53:09 +0100

    song + genre

--- src/model/Song.java
+++ src/model/Song.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Song {
+public class Song implements Comparable<Song>{
+    private String title;
+
+    public Song(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public int compareTo(Song other) {
+        return this.title.compareToIgnoreCase(other.title);
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
